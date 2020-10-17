df0138232ba644ce63ebb7d8e3fe8e8287af1846
Meenatchi Sundaram Muthu Selva Annamalai <msm518@ic.ac.uk>
2020-10-17 21:51:02 +0100

    Moved collapsing of w_prev into the construct method instead of the add_action method

--- algomanim/algoscene.py
+++ algomanim/algoscene.py
@@ -2,24 +2,35 @@
 from manimlib.imports import Scene
 
 class AlgoSceneAction:
-    def __init__(self, act, *args):
+    def __init__(self, act, *args, w_prev=False):
         self.act = act
         self.args = args
+        self.w_prev = w_prev
+
+    def add_args(self, *args):
+        self.args += args
+
+    def run(self):
+        self.act(*self.args)
 
 class AlgoScene(Scene):
     def algoconstruct(self):
         pass
 
     def add_action(self, act, *args, w_prev=False):
-        action = AlgoSceneAction(act, *args)
-        if w_prev and action.act == self.actions[-1].act:
-            self.actions[-1].args += args
-        else:
-            self.actions.append(action)
+        action = AlgoSceneAction(act, *args, w_prev=w_prev)
+        self.actions.append(action)
 
     def construct(self):
         self.actions = []
         self.anim_grps = []
         self.algoconstruct()
-        for action in self.actions:
-            action.act(*action.args)
+        for (i, action) in enumerate(self.actions):
+            for action2 in self.actions[i+1:]:
+                if action2.w_prev:
+                    if action2.act == action.act:
+                        action.add_args(*action2.args)
+                        self.actions.remove(action2)
+                else:
+                    break
+            action.run()
