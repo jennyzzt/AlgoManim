e689cd6e820bd99978f2d83633efbc41e51b6e9a
Avril Wong <aw2718@ic.ac.uk>
2020-10-21 12:36:56 +0100

    Edited video_player field names to follow underscore naming convention

--- gui/video_player.py
+++ gui/video_player.py
@@ -15,7 +15,7 @@ class VideoPlayerWidget(QWidget):
 
         self.video_fp = video_fp
 
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
         # Supporting infrastructure to display media player
         video_item = QGraphicsVideoItem()
@@ -25,76 +25,76 @@ class VideoPlayerWidget(QWidget):
         graphics_view = QGraphicsView(scene)
 
         # Play button
-        self.playButton = QPushButton()
-        self.playButton.setEnabled(False)
-        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.playButton.clicked.connect(self.play)
+        self.play_button = QPushButton()
+        self.play_button.setEnabled(False)
+        self.play_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.play_button.clicked.connect(self.play)
 
         # Video scrubber
-        self.positionSlider = QSlider(Qt.Horizontal)
-        self.positionSlider.setRange(0, 0)
-        self.positionSlider.sliderMoved.connect(self.set_position)
+        self.position_slider = QSlider(Qt.Horizontal)
+        self.position_slider.setRange(0, 0)
+        self.position_slider.sliderMoved.connect(self.set_position)
 
-        self.errorLabel = QLabel()
-        self.errorLabel.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        self.error_label = QLabel()
+        self.error_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
 
         # Wire up media player
-        self.mediaPlayer.setVideoOutput(video_item)
-        self.mediaPlayer.stateChanged.connect(self.media_state_changed)
-        self.mediaPlayer.positionChanged.connect(self.position_changed)
-        self.mediaPlayer.durationChanged.connect(self.duration_changed)
-        self.mediaPlayer.error.connect(self.handle_error)
+        self.media_player.setVideoOutput(video_item)
+        self.media_player.stateChanged.connect(self.media_state_changed)
+        self.media_player.positionChanged.connect(self.position_changed)
+        self.media_player.durationChanged.connect(self.duration_changed)
+        self.media_player.error.connect(self.handle_error)
 
         # Arrange video controls
         control_layout = QHBoxLayout()
         control_layout.setContentsMargins(0, 0, 0, 0)
-        control_layout.addWidget(self.playButton)
-        control_layout.addWidget(self.positionSlider)
+        control_layout.addWidget(self.play_button)
+        control_layout.addWidget(self.position_slider)
 
         # Arrange widget contents
         main_layout = QVBoxLayout()
         main_layout.addWidget(graphics_view)
         main_layout.addLayout(control_layout)
-        main_layout.addWidget(self.errorLabel)
+        main_layout.addWidget(self.error_label)
 
         self.setLayout(main_layout)
 
     def open_video(self):
         file = QDir.current().filePath(str(self.video_fp))
-        self.mediaPlayer.setMedia(
+        self.media_player.setMedia(
             QMediaContent(QUrl.fromLocalFile(file)))
 
         # Enable play button and un-grey it
-        self.playButton.setEnabled(True)
-        self.playButton.setStyleSheet("QPushButton::enabled")
+        self.play_button.setEnabled(True)
+        self.play_button.setStyleSheet("QPushButton::enabled")
 
         # Show first scene of video
-        self.mediaPlayer.play()
-        self.mediaPlayer.pause()
+        self.media_player.play()
+        self.media_player.pause()
 
     def play(self):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.mediaPlayer.pause()
+        if self.media_player.state() == QMediaPlayer.PlayingState:
+            self.media_player.pause()
         else:
-            self.mediaPlayer.play()
+            self.media_player.play()
 
     def media_state_changed(self, state):
-        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-            self.playButton.setIcon(
+        if self.media_player.state() == QMediaPlayer.PlayingState:
+            self.play_button.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPause))
         else:
-            self.playButton.setIcon(
+            self.play_button.setIcon(
                 self.style().standardIcon(QStyle.SP_MediaPlay))
 
     def position_changed(self, position):
-        self.positionSlider.setValue(position)
+        self.position_slider.setValue(position)
 
     def duration_changed(self, duration):
-        self.positionSlider.setRange(0, duration)
+        self.position_slider.setRange(0, duration)
 
     def set_position(self, position):
-        self.mediaPlayer.setPosition(position)
+        self.media_player.setPosition(position)
 
     def handle_error(self):
-        self.playButton.setEnabled(False)
-        self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
+        self.play_button.setEnabled(False)
+        self.error_label.setText("Error: " + self.media_player.errorString())
