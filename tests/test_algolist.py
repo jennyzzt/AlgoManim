e46300d1ee398b58d3c7fd116ac776cc1b8040cc
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-10-26 21:48:34 +0000

    Test: Unit test for AlgoList's metadata find_index

--- tests/test_algolist.py
+++ tests/test_algolist.py
@@ -1,11 +1,13 @@
 # pylint: disable=R0201
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, ANY
 from algomanim.algolist import AlgoList, AlgoListMetadata, Metadata
+from algomanim.algoscene import AlgoScene
 from algomanim.settings import DEFAULT_SETTINGS
 
 test_list = [1, 2, 3]
 algoscene = Mock()
 algoscene.settings = DEFAULT_SETTINGS
+
 
 @patch("algomanim.algolist.VGroup", Mock())
 @patch("algomanim.algolist.TextMobject", Mock())
@@ -25,7 +27,7 @@ class TestAlgoList:
             algoscene.create_play_action(),
             algoscene_action(),
             animated=True,
-            metadata=Metadata(AlgoListMetadata.SWAP)
+            metadata=ANY
         )
 
     @patch("algomanim.algolist.AlgoListNode.show")
@@ -78,5 +80,24 @@ class TestAlgoList:
             algoscene.create_play_action(),
             algoscene_action(),
             animated=True,
-            metadata=Metadata(AlgoListMetadata.CONCAT)
+            metadata=ANY
         )
+
+    def test_find_index_2nd_compare(self):
+        test_algoscene = AlgoScene()
+        algolist = AlgoList(test_algoscene, test_list)
+
+        num_initialisation_action_pairs = len(test_algoscene.action_pairs)
+
+        algolist.compare(0, 1)
+        algolist.swap(0, 1)
+        algolist.compare(1, 2)
+        algolist.swap(1, 2)
+
+        list_index = algolist.find_index(test_algoscene.action_pairs, AlgoListMetadata.COMPARE, 2)
+
+        last_elem = num_initialisation_action_pairs + \
+                    len(algolist.find_index(test_algoscene.action_pairs, AlgoListMetadata.COMPARE, 1)) + \
+                    len(algolist.find_index(test_algoscene.action_pairs, AlgoListMetadata.SWAP, 1))
+
+        assert last_elem == list_index[0]
