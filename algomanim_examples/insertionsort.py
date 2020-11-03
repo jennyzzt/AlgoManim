375cad8d8ff983de04b74dc5c5b66ae34d41ae9d
Avril Wong <aw2718@ic.ac.uk>
2020-11-03 20:15:03 +0000

    Created basic insertion sort example

---
+++ algomanim_examples/insertionsort.py
@@ -0,0 +1,13 @@
+# pylint: skip-file
+
+from algomanim.algoscene import AlgoScene
+from algomanim.algolist import AlgoList
+
+
+class InsertionSortScene(AlgoScene):
+    def algoconstruct(self):
+        algolist = AlgoList(self, [25, 16, 39, 44, 5, 1])
+        for i in range(0, algolist.len()):
+            for j in range(0, i):
+                if algolist.compare(i, j):
+                    algolist.swap(i, j)
