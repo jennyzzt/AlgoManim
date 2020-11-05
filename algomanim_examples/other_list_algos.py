07788823595258eac912a8410719e5e340d848c1
KazBuntu <kyk3218@ic.ac.uk>
2020-11-05 15:43:48 +0000

    Implement add_text function to algolist

--- algomanim_examples/other_list_algos.py
+++ algomanim_examples/other_list_algos.py
@@ -5,21 +5,23 @@ from algomanim.settings import Shape
 
 class FindMaxScene(AlgoScene):
     def algoconstruct(self):
-        algolist = AlgoList(self, [25, 43, 5, 18, 30])
+        algolist = AlgoList(self, [25, 40, 5, 60, 50, 80])
 
         cur_max_idx = 0
+        algolist.add_text("max value: " + str(algolist.get_val(cur_max_idx)))
 
         for i in range(algolist.len()):
             algolist.highlight(cur_max_idx, i)
             if algolist.compare(cur_max_idx, i, highlights=False):
                 algolist.dehighlight(cur_max_idx)
                 cur_max_idx = i
+                algolist.add_text("max value: " + str(algolist.get_val(cur_max_idx)))
             else:
                 algolist.dehighlight(i)
 
     def preconfig(self, settings):
         settings['node_shape'] = Shape.CIRCLE
-        settings['node_size'] = 2.5
+        settings['node_size'] = 1.5
         settings['highlight_color'] = "#33cccc"  # teal
 
 class BinarySearchScene(AlgoScene):
