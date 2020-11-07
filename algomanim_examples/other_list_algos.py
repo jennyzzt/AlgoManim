fe2a3b3f39a01096545b7b5eb3e3086566d5a2b1
Meenatchi Sundaram Muthu Selva Annamalai <msm518@ic.ac.uk>
2020-11-07 22:27:52 +0000

    Added pinning framework

--- algomanim_examples/other_list_algos.py
+++ algomanim_examples/other_list_algos.py
@@ -8,21 +8,45 @@ class FindMaxScene(AlgoScene):
         algolist = AlgoList(self, [25, 40, 5, 60, 50, 80])
 
         cur_max_idx = 0
-        algolist.add_text(f"max value: {str(algolist.get_val(cur_max_idx))}", "max", DOWN)
+        self.insert_pin("max_changed", algolist.get_val(cur_max_idx))
 
         for i in range(algolist.len()):
             if algolist.compare(cur_max_idx, i, highlights=True):
                 cur_max_idx = i
-                algolist.add_text("max value: " + str(algolist.get_val(cur_max_idx)), "max", DOWN)
+                self.insert_pin("max_changed", algolist.get_val(cur_max_idx))
 
     def preconfig(self, settings):
         settings['node_shape'] = Shape.CIRCLE
         settings['node_size'] = 1.5
         settings['highlight_color'] = "#33cccc"  # teal
 
+    def customize(self, action_pairs):
+        # add title to beginning
+        text = TextMobject('Find Maximum Value')
+        text.shift(2 * UP)
+        transform = lambda: Write(text)
+        self.add_transform(0, transform)
 
+        # search for pins that were previously set
+        pins = self.find_pin("max_changed")
+        prev_text = text
+        for pin in pins:
+            # extract val from list of args
+            max_val = pin.get_args()[0]
 
+            # find index to add text transformation to
+            index = pin.get_index()
 
+            # create new text object to morph to
+            new_text = TextMobject(f'Current Max Value: {max_val}')
+            new_text.shift(2 * UP)
+
+            # create transform to be run at that point
+            transform = lambda old_text, new_text: \
+                [FadeOut(old_text), ReplacementTransform(old_text, new_text)]
+            self.add_transform(index, transform, args=[prev_text, new_text])
+
+            prev_text = new_text
 
 class BinarySearchScene(AlgoScene):
     def algoconstruct(self):
