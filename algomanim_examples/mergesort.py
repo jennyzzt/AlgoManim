9a8c9e06684bd725ba1e0d2eef67c0e732be4a51
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-11-09 00:32:48 +0000

    Mergesort algorithm fix

--- algomanim_examples/mergesort.py
+++ algomanim_examples/mergesort.py
@@ -11,6 +11,8 @@ class MergeSortScene(AlgoScene):
 
         self.mergesort(algolist)
 
+    # TODO: Implementation of MergeSort is incorrect. Sample outputl [4,3,1,4] -> [1,4,3,4].
+    #       Merge step (lines 28-31 is incorrect. Needs to be )
     def mergesort(self, algolist):
         if algolist.len() > 1:
             # find middle index
@@ -23,9 +25,23 @@ class MergeSortScene(AlgoScene):
             left = self.mergesort(left)
             right = self.mergesort(right)
 
-            if left.get_val(0) < right.get_val(0):
-                algolist = left.concat(right)
-            else:
-                algolist = right.concat(left)
+            l = 0
+            r = 0
+            self.shift_scene(UP)
+
+            res = AlgoList(self, [])
+            while l < left.len() and r < right.len():
+                if left.get_val(l) < right.get_val(r):
+                    res.append(left.get_val(l))
+                    l += 1
+                else:
+                    res.append(right.get_val(l))
+                    r += 1
+            while l < left.len():
+                res.append(left.get_val(l))
+                l += 1
+            while r < right.len():
+                res.append(right.get_val(l))
+                r += 1
 
         return algolist
