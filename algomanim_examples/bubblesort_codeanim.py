9db0006ced52d700c3a4637b53194a05029e0332
jz14918 <jz14918@ic.ac.uk>
2020-11-14 17:01:27 +0000

    Cleaned up algoconstruct code of inserting code pins

--- algomanim_examples/bubblesort_codeanim.py
+++ algomanim_examples/bubblesort_codeanim.py
@@ -9,24 +9,27 @@ class BubbleSortCodeScene(AlgoScene):
         # get algo source lines
         source_lines, _ = inspect.getsourcelines(self.algo)
 
-        # get and remove redundant leading spaces
-        tab_spacing = (len(source_lines[0]) - len(source_lines[0].lstrip())) * 2
-        source_lines = [line[tab_spacing:] for line in source_lines]
-
-        # insert pin at every alternate source line
         modified_source_lines = []
+
+        # insert pin at the beginning to show all code
+        sourcecode = ''.join([line.replace('\n', '\\n') for line in source_lines])
+        pin_code_source = f'self.insert_pin(\'__codesource__\', \'{sourcecode}\')\n'
+        modified_source_lines.append(pin_code_source)
+
+        # get redundant spacing for first code line that is not def
+        redundant_space_count = len(source_lines[1]) - len(source_lines[1].lstrip())
+        # insert pin at every alternate source line
         for i, line in enumerate(source_lines):
             if i == 0:
                 # do not execute first def line
                 continue
             # get suitable line tab for new code line
-            line_tab = ' ' * (len(line) - len(line.lstrip()))
-            # strip current code line of EOL to be added into pin
-            stripped_line = line.rstrip('\n')
-            # add pin line then add original code line
-            pin_line = f'{line_tab}self.insert_pin(\'__code__\', \'{stripped_line}\')\n'
-            modified_source_lines.append(pin_line)
-            modified_source_lines.append(line)
+            line_tab = ' ' * (len(line) - len(line.lstrip()) - redundant_space_count)
+            # pin index of the code line
+            pin = f'{line_tab}self.insert_pin(\'__codeindex__\', {i})\n'
+            modified_source_lines.append(pin)
+            # add original code back
+            modified_source_lines.append(line[redundant_space_count:])
 
         # get modified source code and execute
         modified_source_str = ''.join(modified_source_lines)
@@ -45,9 +48,5 @@ class BubbleSortCodeScene(AlgoScene):
 
     def customize(self, action_pairs):
         # add code anim text
-        code_pins = self.find_pin('__code__')
-        for pin in code_pins:
-            index = pin.get_index()
-            text = unicode_to_latex(pin.get_args()[0])
-            textobj = TextMobject(text)
-            self.add_transform(index, lambda: FadeIn(textobj))
+        code_pins = self.find_pin('__codesource__')
+        print(len(code_pins))
