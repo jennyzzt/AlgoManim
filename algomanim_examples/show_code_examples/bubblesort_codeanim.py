8fc02eac96bc13742bbab9e9d41d292c4b67fd71
jz14918 <jz14918@ic.ac.uk>
2020-11-18 22:56:42 +0000

    Changed example to the longer list

--- algomanim_examples/show_code_examples/bubblesort_codeanim.py
+++ algomanim_examples/show_code_examples/bubblesort_codeanim.py
@@ -4,7 +4,7 @@ from algomanim.algolist import AlgoList
 
 class DefaultBubbleSortScene(AlgoScene):
     def algo(self):
-        algolist = AlgoList(self, [25, 43, 5])
+        algolist = AlgoList(self, [25, 43, 5, 18, 30])
         swaps_made = True
         while swaps_made:
             swaps_made = False
