33f6372002d929e4d053c64e8174163cf88d94b6
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-11-19 22:05:56 +0000

    AlgoScene small refactor to indicate specific functions

--- algomanim/algoscene.py
+++ algomanim/algoscene.py
@@ -12,7 +12,7 @@ from .metadata_block import MetadataBlock
 from .metadata import Metadata, LowerMetadata
 
 
-# ----- import utility used for code_anim ----- #
+# ----- Import utility used for code_anim ----- #
 Import = namedtuple("Import", ["module", "name", "alias"])
 
 def get_imports(path):
@@ -29,8 +29,8 @@ def get_imports(path):
 
         for node_name in node.names:
             yield Import(module, node_name.name.split('.'), node_name.asname)
-# --------------------------------------------- #
 
+# ------------- AlgoScene functions -------------
 class AlgoScene(MovingCameraScene):
     def __init__(self, **kwargs):
         # Default settings
@@ -53,12 +53,226 @@ class AlgoScene(MovingCameraScene):
 
         MovingCameraScene.__init__(self, **kwargs)
 
-    def is_code_anim(self):
-        return self.settings['code_anim']
+    # ---------- Algorithm default creation ----------- #
+    ''' For user to overwrite '''
+    def algo(self):
+        pass
 
+    # ---------- Customizability utilities ----------- #
     ''' For user to overwrite '''
     def preconfig(self, settings):
         pass
+
+    ''' For user to overwrite '''
+    def customize(self, action_pairs):
+        pass
+
+    def post_config(self, settings):
+        settings.update(self.post_config_settings)
+
+    def insert_pin(self, desc, *args):
+        empty_action = AlgoSceneAction.create_empty_action(list(args))
+        empty_pair = self.add_action_pair(empty_action)
+
+        lower_meta = LowerMetadata(desc, empty_pair)
+        metadata = Metadata(desc)
+        metadata.add_lower(lower_meta)
+
+        self.add_metadata(metadata)
+
+    def find_pin(self, desc):
+        action_pairs = self.find_action_pairs(desc)
+        return action_pairs
+
+    def find_action_pairs(self, method, occurence=None, lower_level=None):
+        action_pairs = []
+        for meta_tree in self.meta_trees:
+            if method == meta_tree.meta_name and (occurence is None or occurence == meta_tree.fid):
+                if lower_level:
+                    for lower in meta_tree.children:
+                        if lower_level == lower.meta_name:
+                            action_pairs.append(lower.action_pair)
+                else:
+                    for action_pair in meta_tree.get_all_action_pairs():
+                        action_pairs.append(action_pair)
+        return action_pairs
+
+    def add_transform(self, index, transform, args=[], metadata=None): # pylint: disable=W0102
+        anim_action = self.create_play_action(AlgoTransform(args, transform=transform))
+        action_pair = AlgoSceneActionPair(anim_action, anim_action)
+        self.insert_action_pair(action_pair, index)
+
+        if metadata is None:
+            curr_metadata = Metadata('custom')
+            lower_meta = LowerMetadata('custom', action_pair)
+            curr_metadata.add_lower(lower_meta)
+
+            self.add_metadata(curr_metadata)
+        else:
+            self.add_metadata(metadata)
+
+    # ------------ Text customizability --------------
+    # Add a text object and the Write transform
+    # Returns the created text object
+    def add_text(self, text, index, position=UP):
+        text = TextMobject(text)
+        text.shift(position)
+        transform = lambda: Write(text)
+        self.add_transform(index, transform)
+        return text
+
+    # Edit existing text objects via Manim's ReplacementTransform
+    # Requires the previous text object to be edited
+    # Returns the replacement text object
+    def change_text(self, new_text_string, old_text_object, index=None):
+        position = old_text_object.get_center()
+        new_text_object = TextMobject(new_text_string)
+        new_text_object.shift(position)
+
+        # Create the transform to be run at that point
+        transform = lambda old_text, new_text: \
+            [FadeOut(old_text), ReplacementTransform(old_text, new_text)]
+        self.add_transform(index, transform, args=[old_text_object, new_text_object])
+        return new_text_object
+
+    # FadeOut an existing text object
+    def remove_text(self, old_text_object, index):
+        transform = lambda: FadeOut(old_text_object)
+        self.add_transform(index, transform)
+
+    # ------------ Scene customizability --------------
+    def shift_scene(self, vector, metadata=None, w_prev=False):
+        first = True
+
+        for algo_obj in self.algo_objs:
+            # Shift all items UP
+            if first:
+                algo_obj.set_next_to(algo_obj, vector, metadata=metadata, animated=True,
+                    w_prev=w_prev)
+                first = False
+            else:
+                algo_obj.set_next_to(algo_obj, vector, metadata=metadata, animated=True,
+                    w_prev=True)
+
+    def skip(self, start, end=None):
+        if end is None:
+            end = len(self.action_pairs)
+
+        for action_pair in self.action_pairs[start:end]:
+            action_pair.skip()
+
+    def fast_forward(self, start, end=None, speed_up=2):
+        if end is None:
+            end = len(self.action_pairs)
+
+        for action_pair in self.action_pairs[start:end]:
+            action_pair.fast_forward(speed_up)
+
+    def add_slide(self, text, index, text_position=ORIGIN, duration=1.0):
+        self.add_fade_out_all(index)
+        text_anim = self.add_text(text, index + 1, position=text_position)
+        self.add_wait(index + 2, wait_time=duration)
+        self.remove_text(text_anim, index + 3)
+        self.add_fade_in_all(index + 4)
+
+    def add_fade_out_all(self, index):
+        anim_action = self.create_play_action(AlgoTransform([self], transform=fade_out_transform))
+        action_pair = AlgoSceneActionPair(anim_action, anim_action)
+        self.insert_action_pair(action_pair, index)
+
+        curr_metadata = Metadata('fade_out')
+        lower_meta = LowerMetadata('fade_out', action_pair)
+        curr_metadata.add_lower(lower_meta)
+
+        self.add_metadata(curr_metadata)
+
+    def add_fade_in_all(self, index):
+        anim_action = self.create_play_action(AlgoTransform([self], transform=fade_in_transform))
+        action_pair = AlgoSceneActionPair(anim_action, anim_action)
+        self.insert_action_pair(action_pair, index)
+
+        curr_metadata = Metadata('fade_in')
+        lower_meta = LowerMetadata('fade_in', action_pair)
+        curr_metadata.add_lower(lower_meta)
+
+        self.add_metadata(curr_metadata)
+
+    def add_wait(self, index, wait_time=1):
+        anim_action = AlgoSceneAction(self.wait, AlgoTransform([wait_time]))
+        # Using a dummy function to skip wait
+        static_action = AlgoSceneAction.create_empty_action()
+        action_pair = AlgoSceneActionPair(anim_action, static_action)
+        self.insert_action_pair(action_pair, index)
+
+        curr_metadata = Metadata('wait')
+        lower_meta = LowerMetadata('wait', action_pair)
+        curr_metadata.add_lower(lower_meta)
+
+        self.add_metadata(curr_metadata)
+
+    def add_clear(self, index):
+        action_pair = AlgoSceneAction.create_static_action(self.clear)
+        self.insert_action_pair(AlgoSceneActionPair(action_pair), index)
+
+        curr_metadata = Metadata('clear')
+        lower_meta = LowerMetadata('clear', action_pair)
+        curr_metadata.add_lower(lower_meta)
+
+        self.add_metadata(curr_metadata)
+
+    # ----- Data Structure utilities -------
+    # Add item so they can subscribe themselves to scene transformations like Shifts
+    def track_algoitem(self, algo_item):
+        self.algo_objs.append(algo_item)
+
+    def untrack_algoitem(self, algo_item):
+        if algo_item in self.algo_objs:
+            self.algo_objs.remove(algo_item)
+
+    def create_play_action(self, transform, w_prev=False):
+        return AlgoSceneAction(
+            self.play, transform,
+            w_prev=w_prev, can_set_runtime=True
+        )
+
+    def add_action_pair(self, anim_action, static_action=None, animated=True, index=None):
+        pair = AlgoSceneActionPair(anim_action, static_action,
+                                   run_time=None if animated else 0)
+        self.insert_action_pair(pair, index)
+        return pair
+
+    def insert_action_pair(self, action_pair, index=None):
+        if index is not None:
+            self.push_back_action_pair_indices(index)
+        else:
+            index = len(self.action_pairs)
+
+        action_pair.attach_index(index)
+        self.action_pairs.insert(index, action_pair)
+
+    def push_back_action_pair_indices(self, index):
+        for action_pair in self.action_pairs[index:]:
+            action_pair.attach_index(action_pair.get_index() + 1)
+
+    def add_static(self, index, static_fn, args=[], metadata=None):  # pylint: disable=W0102
+        static_action = AlgoSceneAction.create_static_action(static_fn, args)
+        action_pair = AlgoSceneActionPair(static_action, static_action)
+        self.insert_action_pair(action_pair, index)
+
+        if metadata is None:
+            curr_metadata = Metadata('custom')
+            lower_meta = LowerMetadata('custom', action_pair)
+            curr_metadata.add_lower(lower_meta)
+            self.add_metadata(curr_metadata)
+        else:
+            self.add_metadata(metadata)
+
+    def add_metadata(self, metadata):
+        self.meta_trees.append(metadata)
+
+    # ------------ AlgoScene Internal Wiring --------------
+    def is_code_anim(self):
+        return self.settings['code_anim']
 
     def algo_codeanim(self):
         # import necessary modules
@@ -123,10 +337,6 @@ class AlgoScene(MovingCameraScene):
         else:
             self.algo()
 
-    ''' For user to overwrite '''
-    def algo(self):
-        pass
-
     def customize_codeanim(self):
         # ----- helper static fns ----- #
         def zoom_out():
@@ -185,183 +395,6 @@ class AlgoScene(MovingCameraScene):
             self.customize_codeanim()
         # Run user-defined customize fn
         self.customize(action_pairs)
-
-    ''' For user to overwrite '''
-    def customize(self, action_pairs):
-        pass
-
-    def post_config(self, settings):
-        settings.update(self.post_config_settings)
-
-    def create_play_action(self, transform, w_prev=False):
-        return AlgoSceneAction(
-            self.play, transform,
-            w_prev=w_prev, can_set_runtime=True
-        )
-
-    def add_action_pair(self, anim_action, static_action=None, animated=True, index=None):
-        pair = AlgoSceneActionPair(anim_action, static_action,
-                                   run_time=None if animated else 0)
-        self.insert_action_pair(pair, index)
-        return pair
-
-    def insert_action_pair(self, action_pair, index=None):
-        if index is not None:
-            self.push_back_action_pair_indices(index)
-        else:
-            index = len(self.action_pairs)
-
-        action_pair.attach_index(index)
-        self.action_pairs.insert(index, action_pair)
-
-    def add_metadata(self, metadata):
-        self.meta_trees.append(metadata)
-
-    # Add item so they can subscribe themselves to scene transformations like Shifts
-    def track_algoitem(self, algo_item):
-        self.algo_objs.append(algo_item)
-
-    def untrack_algoitem(self, algo_item):
-        if algo_item in self.algo_objs:
-            self.algo_objs.remove(algo_item)
-
-    def shift_scene(self, vector, metadata=None, w_prev=False):
-        first = True
-
-        for algo_obj in self.algo_objs:
-            # Shift all items UP
-            if first:
-                algo_obj.set_next_to(algo_obj, vector, metadata=metadata, animated=True,
-                    w_prev=w_prev)
-                first = False
-            else:
-                algo_obj.set_next_to(algo_obj, vector, metadata=metadata, animated=True,
-                    w_prev=True)
-
-    def skip(self, start, end=None):
-        if end is None:
-            end = len(self.action_pairs)
-
-        for action_pair in self.action_pairs[start:end]:
-            action_pair.skip()
-
-    def push_back_action_pair_indices(self, index):
-        for action_pair in self.action_pairs[index:]:
-            action_pair.attach_index(action_pair.get_index() + 1)
-
-    def add_transform(self, index, transform, args=[], metadata=None): # pylint: disable=W0102
-        anim_action = self.create_play_action(AlgoTransform(args, transform=transform))
-        action_pair = AlgoSceneActionPair(anim_action, anim_action)
-        self.insert_action_pair(action_pair, index)
-
-        if metadata is None:
-            curr_metadata = Metadata('custom')
-            lower_meta = LowerMetadata('custom', action_pair)
-            curr_metadata.add_lower(lower_meta)
-
-            self.add_metadata(curr_metadata)
-        else:
-            self.add_metadata(metadata)
-
-    # Convenience function to add a text object and the Write transform
-    # Returns the created text object
-    def add_text(self, text, index, position=UP):
-        text = TextMobject(text)
-        text.shift(position)
-        transform = lambda: Write(text)
-        self.add_transform(index, transform)
-        return text
-
-    # Convenience function to edit existing text objects via a ReplacementTransform
-    # Requires the previous text object to be edited
-    # Returns the replacement text object
-    def change_text(self, new_text_string, old_text_object, index=None):
-        position = old_text_object.get_center()
-        new_text_object = TextMobject(new_text_string)
-        new_text_object.shift(position)
-
-        # Create the transform to be run at that point
-        transform = lambda old_text, new_text: \
-            [FadeOut(old_text), ReplacementTransform(old_text, new_text)]
-        self.add_transform(index, transform, args=[old_text_object, new_text_object])
-        return new_text_object
-
-    # Convenience function to FadeOut an existing text object
-    def remove_text(self, old_text_object, index):
-        transform = lambda: FadeOut(old_text_object)
-        self.add_transform(index, transform)
-
-    def add_static(self, index, static_fn, args=[], metadata=None): # pylint: disable=W0102
-        static_action = AlgoSceneAction.create_static_action(static_fn, args)
-        action_pair = AlgoSceneActionPair(static_action, static_action)
-        self.insert_action_pair(action_pair, index)
-
-        if metadata is None:
-            curr_metadata = Metadata('custom')
-            lower_meta = LowerMetadata('custom', action_pair)
-            curr_metadata.add_lower(lower_meta)
-            self.add_metadata(curr_metadata)
-        else:
-            self.add_metadata(metadata)
-
-    def add_slide(self, text, index, text_position=ORIGIN, duration=1.0):
-        self.add_fade_out_all(index)
-        text_anim = self.add_text(text, index + 1, position=text_position)
-        self.add_wait(index + 2, wait_time=duration)
-        self.remove_text(text_anim, index + 3)
-        self.add_fade_in_all(index + 4)
-
-    def add_fade_out_all(self, index):
-        anim_action = self.create_play_action(AlgoTransform([self], transform=fade_out_transform))
-        action_pair = AlgoSceneActionPair(anim_action, anim_action)
-        self.insert_action_pair(action_pair, index)
-
-        curr_metadata = Metadata('fade_out')
-        lower_meta = LowerMetadata('fade_out', action_pair)
-        curr_metadata.add_lower(lower_meta)
-
-        self.add_metadata(curr_metadata)
-
-    def add_fade_in_all(self, index):
-        anim_action = self.create_play_action(AlgoTransform([self], transform=fade_in_transform))
-        action_pair = AlgoSceneActionPair(anim_action, anim_action)
-        self.insert_action_pair(action_pair, index)
-
-        curr_metadata = Metadata('fade_in')
-        lower_meta = LowerMetadata('fade_in', action_pair)
-        curr_metadata.add_lower(lower_meta)
-
-        self.add_metadata(curr_metadata)
-
-    def add_wait(self, index, wait_time=1):
-        anim_action = AlgoSceneAction(self.wait, AlgoTransform([wait_time]))
-        # Using a dummy function to skip wait
-        static_action = AlgoSceneAction.create_empty_action()
-        action_pair = AlgoSceneActionPair(anim_action, static_action)
-        self.insert_action_pair(action_pair, index)
-
-        curr_metadata = Metadata('wait')
-        lower_meta = LowerMetadata('wait', action_pair)
-        curr_metadata.add_lower(lower_meta)
-
-        self.add_metadata(curr_metadata)
-
-    def add_clear(self, index):
-        action_pair = AlgoSceneAction.create_static_action(self.clear)
-        self.insert_action_pair(AlgoSceneActionPair(action_pair), index)
-
-        curr_metadata = Metadata('clear')
-        lower_meta = LowerMetadata('clear', action_pair)
-        curr_metadata.add_lower(lower_meta)
-
-        self.add_metadata(curr_metadata)
-
-    def fast_forward(self, start, end=None, speed_up=2):
-        if end is None:
-            end = len(self.action_pairs)
-
-        for action_pair in self.action_pairs[start:end]:
-            action_pair.fast_forward(speed_up)
 
     def create_animation_blocks(self, action_pairs, anim_blocks): # pylint: disable=R0201
         # convert action_pairs into anim_blocks
@@ -434,33 +467,6 @@ class AlgoScene(MovingCameraScene):
 
         # some metadata might be added out of order, sort the blocks by start_time
         self.metadata_blocks.sort(key = lambda meta_block: meta_block.start_time)
-
-    def insert_pin(self, desc, *args):
-        empty_action = AlgoSceneAction.create_empty_action(list(args))
-        empty_pair = self.add_action_pair(empty_action)
-
-        lower_meta = LowerMetadata(desc, empty_pair)
-        metadata = Metadata(desc)
-        metadata.add_lower(lower_meta)
-
-        self.add_metadata(metadata)
-
-    def find_pin(self, desc):
-        action_pairs = self.find_action_pairs(desc)
-        return action_pairs
-
-    def find_action_pairs(self, method, occurence=None, lower_level=None):
-        action_pairs = []
-        for meta_tree in self.meta_trees:
-            if method == meta_tree.meta_name and (occurence is None or occurence == meta_tree.fid):
-                if lower_level:
-                    for lower in meta_tree.children:
-                        if lower_level == lower.meta_name:
-                            action_pairs.append(lower.action_pair)
-                else:
-                    for action_pair in meta_tree.get_all_action_pairs():
-                        action_pairs.append(action_pair)
-        return action_pairs
 
     def construct(self):
         Metadata.reset_counter()
