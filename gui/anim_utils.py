fa547ec6437561760cc717a037630fb828c47586
Avril Wong <aw2718@ic.ac.uk>
2020-11-19 14:26:42 +0000

    Removed display of w_prev flag on anim blocks

--- gui/anim_utils.py
+++ gui/anim_utils.py
@@ -5,7 +5,5 @@ def format_anim_block_str(metablock, sep='\n'):
     # extract metadata from the block
     meta = metablock.metadata
 
-    w_prev = f"w_prev{sep}" if meta.w_prev else ""
     return f"{meta.meta_name}{sep}" \
-           f"{w_prev}" \
            f"#{meta.fid}"
