47fca7e3b2584b77a54484d2d24b344a663a2d96
Avril Wong <aw2718@ic.ac.uk>
2020-11-20 22:11:56 +0000

    pylint fixes

--- algomanim/empty_animation.py
+++ algomanim/empty_animation.py
@@ -4,21 +4,21 @@ from algomanim.metadata_block import MetadataBlock
 
 class EmptyMetadataBlock(MetadataBlock):
     def __init__(self, index):
-        super(EmptyMetadataBlock, self).__init__(EmptyMetadata(), [], -1, 0.5)
+        super().__init__(EmptyMetadata(), [], -1, 0.5)
         self.index = index
 
 
 class EmptyMetadata(Metadata):
     def __init__(self):
         # make it animated so it will be displayed
-        super(EmptyMetadata, self).__init__("custom_animation", animated=True)
+        super().__init__("custom_animation", animated=True)
 
     def desc(self, sep='\n'):
         return "Add custom animation"
 
 
 def is_empty_anim(anim):
-    return type(anim) == EmptyMetadataBlock
+    return isinstance(anim, EmptyMetadataBlock)
 
 
 def empty_animation(position):
