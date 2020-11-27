65f4faad210176880df9ecb0a0e773e32a615786
TerenceHernandez <t.na.m.hernandez@gmail.com>
2020-11-27 15:51:12 +0000

    Example customisations

--- algomanim_examples/binaryheap_algorithms/heap_sort.py
+++ algomanim_examples/binaryheap_algorithms/heap_sort.py
@@ -7,15 +7,17 @@ class HeapSort(AlgoScene):
 
     # pylint: disable=W0613
     def algo(self):
-
-        heap = AlgoBinaryHeap(self, [4, 3, 2, 1], heap_type="min-heap")
+        self.insert_pin("start_build_heap")
+        heap = AlgoBinaryHeap(self, [6, 5, 4, 3, 2, 1], heap_type="min-heap")
 
         self.insert_pin("finished_build_heap")
         sorted_list = AlgoList(self, [], displacement=3.5 * DOWN)
-
+        self.insert_pin("pop_off_items")
         while heap.peek() is not None:
+            self.insert_pin("top_element", heap.peek_item())
             val = heap.pop()
             sorted_list.append(val)
+        self.insert_pin("algorithm_finished")
 
     def preconfig(scene, settings):
         settings['node_size'] = 0.5
@@ -23,7 +25,34 @@ class HeapSort(AlgoScene):
         settings['highlight_color'] = "#e74c3c"  # red
 
     def customize(self, action_pairs):
-        pass
+
+        # I want to have a introduction title before I start the algorithm
+        start_build_heap = self.find_pin("start_build_heap")[0]
+        bh_idx = start_build_heap.get_index()
+        title_text = self.add_text("First build a Min-heap of the list", bh_idx, position=2 * UP)
+        self.add_wait(bh_idx + 1, wait_time=0.25)
+
+        # I want to change the title when i finish building the heap
+        finished_build_heap = self.find_pin("finished_build_heap")[0]
+        fbp_idx = finished_build_heap.get_index()
+
+        title_text = self.change_text("We have finished building the heap", title_text, fbp_idx)
+        self.add_wait(fbp_idx + 1, wait_time=1)
+
+        # I want to change the title when I start to build the sorted list
+        start_popping = self.find_pin("pop_off_items")[0]
+        sp_idx = start_popping.get_index()
+
+        title_text = self.change_text("Pop off the elements one by one", title_text, sp_idx)
+
+        # I want to say that we have ended
+        finished = self.find_pin("algorithm_finished")[0]
+        f_idc = finished.get_index()
+
+        self.change_text("We have a sorted list!", title_text, f_idc)
+
+
+
 
 
 
--- algomanim_examples/binaryheap_algorithms/sliding_window_maximum.py
+++ algomanim_examples/binaryheap_algorithms/sliding_window_maximum.py
@@ -14,17 +14,21 @@ class SlidingWindowMaximum(AlgoScene):
             input = AlgoList(self, list_input, displacement=2.5 * DOWN)
             res = AlgoList(self, arr=[], displacement=3.5 * DOWN)
 
+            self.insert_pin("build_initial_window")
             for i in range(k):
                 heap.insert(list_input[i])
                 self.insert_pin('window', input.nodes[0], input.nodes[i])
 
+            self.insert_pin("add_peek_for_each")
             res.append(heap.peek())
+            self.insert_pin("build_windows")
             for i in range(k, len(list_input)):
                 self.insert_pin('window', input.nodes[i-k + 1], input.nodes[i])
                 heap.remove(list_input[i-k])
                 heap.insert(list_input[i])
                 res.append(heap.peek())
 
+            self.insert_pin("algorithm_finished")
             return res
 
         sliding_window_maximum(list_input=[3, 6, 1, 8, 3, 9, 0, 1], k=3)
@@ -35,6 +39,33 @@ class SlidingWindowMaximum(AlgoScene):
         settings['highlight_color'] = "#e74c3c"  # red
 
     def customize(self, action_pairs):
+
+        # I want to have a introduction title for the initial window
+        bi_window = self.find_pin("build_initial_window")[0]
+        bi_idx = bi_window.get_index()
+        title_text = self.add_text("First build our initial subarray", bi_idx, position=2 * UP)
+        self.add_wait(bi_idx + 1, wait_time=0.25)
+
+        # I want to change the title when I first add to the result
+        first_add = self.find_pin("add_peek_for_each")[0]
+        fa_idx = first_add.get_index()
+
+        title_text = self.change_text("Add the peek of each subarray!", title_text, fa_idx)
+        self.add_wait(fa_idx + 1, wait_time=1)
+
+        # I want to change the title when i start building all windows
+        build_windows = self.find_pin("build_windows")[0]
+        build_idx = build_windows.get_index()
+
+        title_text = self.change_text("Create the rest of the subarrays", title_text, build_idx)
+        self.add_wait(build_idx + 1, wait_time=1)
+
+        # I want to say that we have ended
+        finished = self.find_pin("algorithm_finished")[0]
+        f_idc = finished.get_index()
+
+        self.change_text("We have found the maximum of each subarray!", title_text, f_idc)
+
         # Add sliding window to the input list
         windows = self.find_pin('window')
         self.sub_arr = None
@@ -42,9 +73,9 @@ class SlidingWindowMaximum(AlgoScene):
             index = window.get_index()
             first_node = window.get_args()[0]
             last_node = window.get_args()[1]
-            self.add_static(index, self.update_surrounding_box, [first_node, last_node])
+            self.add_static(index, self.update_surrounding_box, [first_node, last_node])\
 
-    # ------ Helper functions for my own Manim animations ----- #
+    # ------ Helper function for my own Manim animations ----- #
     def update_surrounding_box(self, first_node, last_node):
         old_box = self.sub_arr
         new_box = SurroundingRectangle(VGroup(first_node.grp, last_node.grp))
